8a9604f2a04eb5849ff3ca3304c8c20a17fe367d
royroycat <royroycat@gmail.com>
2014-10-07 14:59:05 +0800

    Update AJSITunesResult.m

--- AJSITunesAPI/AJSITunesResult.m
+++ AJSITunesAPI/AJSITunesResult.m
@@ -111,7 +111,9 @@ NSString *const AJSITunesAttributeTvSeasonTerm = @"tvSeasonTerm";
               @"previewURL" : @"previewUrl",
               @"viewURL" : @"trackViewUrl",
               @"duration" : @"trackTimeMillis",
-              @"releaseDate" : @"releaseDate"};
+              @"releaseDate" : @"releaseDate"
+              @"imageURLString" : @"artworkUrl100"
+    };
 }
 
 
